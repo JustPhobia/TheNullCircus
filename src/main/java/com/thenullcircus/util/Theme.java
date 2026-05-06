57070320727f96ce6f593cd54f1ddf1edf1e5038
Christan Coskey <christan1234@gmail.com>
2026-05-06 11:39:58 +0200

    added LoginForm and Theme files and started working on styling in theme file

---
+++ src/main/java/com/thenullcircus/util/Theme.java
@@ -0,0 +1,14 @@
+package com.thenullcircus.util;
+
+import java.awt.*;
+
+public class Theme {
+
+    //these are the colors for our app
+    public static final Color PRIMARY = new Color(0xFF0000);
+    public static final Color PRIMARY_HOVER = new Color(0xFFFF00);
+    public static final Color BACKGROUND = new Color(0xFF00FF);
+    public static final Color SURFACE = Color.WHITE;
+    public static final Color TEXT_PRIMARY = new Color(0xFF00FF);
+
+}
---
+++ src/main/java/com/thenullcircus/view/LoginForm.java
@@ -0,0 +1,30 @@
+package com.thenullcircus.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends BasePanel {
+    private JPanel mainPanel;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JLabel errorLabel;
+    private JLabel welcomeText;
+    private JLabel usernameTitle;
+    private JLabel passwordTitle;
+
+    public LoginForm(MainWindow mainWindow) {
+        super(mainWindow);
+        setLayout(new BorderLayout());
+        add(mainPanel, BorderLayout.CENTER);
+
+
+    }
+
+    public void styleComponents(){
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(40, 50, 40, 50));
+
+        //this is for the title
+//        welcomeText.setFont(new);
+    }
+}
