968050c8c74597c4d2ea7b3061e519fb1ddf43c8
Christan Coskey <christan1234@gmail.com>
2026-05-06 11:09:26 +0200

    added the main App class App.java and added the structure for UI in MainWindow

---
+++ src/main/java/com/thenullcircus/view/App.java
@@ -0,0 +1,17 @@
+package com.thenullcircus.view;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow window = new MainWindow();
+                window.set
+            }
+        })
+
+    }
+}
---
+++ src/main/java/com/thenullcircus/view/MainWindow.java
@@ -0,0 +1,48 @@
+package com.thenullcircus.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+
+    public static final String LOGIN_PANEL = "LOGIN";
+    public static final String REGISTRATION_PANEL = "REGISTER";
+    public static final String MAIN_FEED_PANEL = "MAIN_FEED";
+    public static final String POST_CREATION_PANEL = "POST_CREATION";
+    public static final String DASHBOARD_PANEL = "DASHBOARD";
+    public static final String SETTINGS_PANEL = "SETTINGS";
+    public static final String MODERATION_PANEL = "MODERATION";
+
+    public MainWindow() {
+        initFrame();
+        initCards();
+    }
+
+    public void initFrame() {
+        setTitle("My App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(800, 600));
+        setLocationRelativeTo(null); //this will center our frame
+    }
+
+    public void initCards(){
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        //we will add our pages here
+
+        setContentPane(cardPanel);
+        pack();
+
+        cardLayout.show(cardPanel, LOGIN_PANEL);
+    }
+
+    public void navigateTo(String panelName){
+        cardLayout.show(cardPanel, panelName);
+    }
+
+
+}
