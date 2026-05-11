c302e0ff626d5a82b7c1f0319d6d4ea2f47a76e3
JustPhobia <jarryd.lautenbach3@gmail.com>
2026-05-11 10:17:20 +0200

    security: add BCrypt password hashing to AuthService

--- src/main/java/com/thenullcircus/controller/services/AuthService.java
+++ src/main/java/com/thenullcircus/controller/services/AuthService.java
@@ -3,6 +3,7 @@ package com.thenullcircus.controller.services;
 import com.thenullcircus.dao.UserDao;
 import com.thenullcircus.dao.UserDaoImpl;
 import com.thenullcircus.model.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class AuthService {
 
@@ -18,7 +19,7 @@ public class AuthService {
             return null;
         }
 
-        if (user.getPassword().equals(password)) {
+        if (BCrypt.checkpw(password, user.getPassword())) {
             return user;
         }
         return null;
@@ -28,6 +29,7 @@ public class AuthService {
         if (userDao.findByUsername(user.getUsername()) != null){
             return false;
         }
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
         boolean result = userDao.registerUser(user);
 
         System.out.println("DAO registration result: " + result);
